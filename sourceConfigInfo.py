5342422b29a0bc11c0d1b7b9d01966debf231e4a
Leland Batey <lelandbatey@gmail.com>
2012-11-03 01:17:34 -0700

    Now automatically searches for cfg files
    Searches for CFG files in the Steam folder, then asks the user which one
    they want to analyze.

--- sourceConfigInfo.py
+++ sourceConfigInfo.py
@@ -30,6 +30,10 @@ config file and tell you:
 ###############
 #   Changes   #
 ###############
+v0.03
+Additions
+ - Automatically finds config.cfg files
+ - Bit better modulization now
 
 v0.02
 Additions
@@ -41,26 +45,28 @@ First Version
  Bugs:
  - My program is OBVIOUSLY perfect. Nothing to change here.
 '''
+import os
+import fnmatch
 
-def main():
-    bindNames = "\ ins del home end pgdn pgup kp_ins kp_del kp_end kp_downarrow kp_pgdn kp_leftarrow kp_5 kp_rightarrow kp_home kp_uparrow kp_pgup kp_slash * kp_minus kp_plus kp_enter a b c d e f g h i j k l m n o p q r s t u v w x y z , . /  ; ’ [ ] - = ~ leftarrow rightarrow uparrow downarrow enter space shift ctrl alt backspace tab escape pause mouse1 mouse2 mouse3 mouse4 mouse5 mwheelup mwheeldown 0 1 2 3 4 5 6 7 8 9".split(' ')
-    # The above monstrosity is a giant string of all the possible keys that you can bind to. This is a list of the names that the source engine assigns to these keys, so they may be a bit confusing to read.
+def parseConfig(fileFolder,returnList=False):
+    bindNames = "\\ ins del home end pgdn pgup kp_ins \
+kp_del kp_end kp_downarrow kp_pgdn kp_leftarrow kp_5 kp_rightarrow \
+kp_home kp_uparrow kp_pgup kp_slash * kp_minus kp_plus kp_enter a b\
+ c d e f g h i j k l m n o p q r s t u v w x y z , . /  ; ’ [ ] - = \
+~ leftarrow rightarrow uparrow downarrow enter space shift ctrl alt\
+ backspace tab escape pause mouse1 mouse2 mouse3 mouse4 mouse5 mwheelup\
+ mwheeldown 0 1 2 3 4 5 6 7 8 9".split(' ')
+    # These are all the keys that can be have bindings in the Source engine.
+    # The names are the ones given by the Source engine, so they may be a bit strange.
+
     doneFlag = False
-    configFolder = input("Paste the path to the 'cfg' folder in here(Example: 'C:\Program Files\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\cfg': ")
-
-    try:
-        configFile = open(configFolder+'\\config.cfg', 'r')
-    except Exception as e:
-        print(str(e))
-    
+    configFile = open(fileFolder+'\\config.cfg', 'r')
     keyBindList = []
     bindList = []
-
     
     while not doneFlag:
         curLine = configFile.readline()
-        
-        if len(curLine) > 0 and curLine.count('bind ') > 0:
+        if len(curLine) and curLine.count('bind ') and not curLine.count('\\\\'):
             
             buildLine = ""
             curLine = curLine.lower()
@@ -73,7 +79,6 @@ def main():
             # Adds the key being bound and the bound-value into differnt arrays for each   
             keyBindList.append(buildLine.split("@")[0])
             bindList.append(buildLine.split("@")[1])
-            
         elif len(curLine) == 0:
             doneFlag = True
             
@@ -82,14 +87,53 @@ def main():
     for keyBind in range(0,len(keyBindList),1):
         buildLine += 'The key {0:^16} is bound to {1:<22}\n'.format('"'+keyBindList[keyBind]+'"','"'+bindList[keyBind]+'"')
 
-        
     for keyBind in bindNames:
         if keyBind in keyBindList:
             pass
         else:
             buildLine += "The {0:^18} key is unbound\n".format('"'+keyBind+'"')
     
-    print(buildLine)
+    return(buildLine)
 	
+
+	
+def getSourceDirectories():
+    # This searches the Program Files 
+    
+    rootPath = os.listdir('C:\\')
+    doneFlag = False
+    if 'Program Files (x86)' in os.listdir('C:\\'):
+        is64bit = True
+    configLocation = []
+
+    # Creates an index of all the folders that contain 'config.cfg' files
+    if is64bit:
+        for root, dirs, files in os.walk('C:\\Program Files (x86)\\Steam\\steamapps\\'):
+            for filename in fnmatch.filter(files, 'config.cfg'):
+                configLocation.append(root+'\\')
+    else:
+        for root, dirs, files in os.walk('C:\\Program Files\\Steam\\steamapps\\'):
+            for filename in fnmatch.filter(files, 'config.cfg'):
+                configLocation.append(root+'\\')
+
+    return(configLocation)
+
+def chooseConfigDir(locationArray):
+    toReturn = ""
+
+    print("Which file do you want to analyze?: ")
+    for index, path in enumerate(locationArray):
+        print("["+str(index+1)+"]"+"  "+path)
+    indexChosen = input("Enter the config folder to be analyzed (default - 1): ") or 0
+    if indexChosen: indexChosen = int(indexChosen)-1
+    toReturn = locationArray[indexChosen]
+
+    return toReturn
+
+def main():
+    locationsArray = getSourceDirectories()
+    chosenLocation = chooseConfigDir(locationsArray)
+    print(chosenLocation)
+    parsedData = parseConfig(chosenLocation)
+    print(parsedData)
 main()
-	
